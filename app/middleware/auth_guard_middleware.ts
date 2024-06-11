c2e40ccce5f8341bd7718cc0b36b2d7b6d543552
aaron <aaronlyn88@gmail.com>
2024-06-11 14:35:29 +0800

    feat: added deleted at attribute

--- app/middleware/auth_guard_middleware.ts
+++ app/middleware/auth_guard_middleware.ts
@@ -1,28 +1,25 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import lucia from '#services/lucia_service';
-import { Exception } from '@adonisjs/core/exceptions';
+// import { Exception } from '@adonisjs/core/exceptions';
 import UnAuthorizedException from '#exceptions/un_authorized_exception';
+import { Role } from '@prisma/client';
+
+interface MiddlewareOptions {
+  roles?: Role[];
+}
 
 export default class AuthGuardMiddleware {
-  async handle(ctx: HttpContext, next: NextFn) {
-    /**
-     * Middleware logic goes here (before the next call)
-     */
-
+  async handle(ctx: HttpContext, next: NextFn, options?: MiddlewareOptions) {
     const sessionId = lucia.readSessionCookie(ctx.request.headers().cookie ?? "");
     if (!sessionId) {
       ctx.request.user = null;
       ctx.request.session = null;
-      throw new UnAuthorizedException()
+      throw new UnAuthorizedException();
     }
 
     const { session, user } = await lucia.validateSession(sessionId);
     if (session && session.fresh) {
-      // reply.header(
-      //   "Set-Cookie",
-      //   lucia.createSessionCookie(session.id).serialize(),
-      // );
       ctx.response.header(
         "Set-Cookie",
         lucia.createSessionCookie(session.id).serialize()
@@ -30,19 +27,28 @@ export default class AuthGuardMiddleware {
     }
 
     if (!session) {
-      // reply.header("Set-Cookie", lucia.createBlankSessionCookie().serialize());
       ctx.response.header(
         "Set-Cookie",
         lucia.createBlankSessionCookie().serialize()
       );
     }
+    // if there is no user found but a role prop exist
+
+    if (!user) {
+      throw new UnAuthorizedException();
+    }
+
+    // Check if the user has the required role
+    if (options?.roles && !options.roles.includes(user.role)) {
+      throw new UnAuthorizedException('Permission denied');
+    }
+
     ctx.request.user = user;
     ctx.request.session = session;
 
-    /**
-     * Call next method in the pipeline and return its output
-     */
-    const output = await next()
-    return output
+
+    const output = await next();
+    return output;
   }
 }
+
